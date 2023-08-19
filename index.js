8537c8f694bd7172f901ecc60bdbd899b9fe9950
Stargazing-11 <samuelkifle199314@gmail.com>
2023-08-19 16:08:09 +0300

    add agoratokenserver

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const { RtcTokenBuilder, RtcRole } = require("agora-access-token");
+require("dotenv").config();
+
+const PORT = 8080;
+const APP_ID = process.env.APP_ID;
+const APP_CERTIFICATE = process.env.APP_CERTIFICATE;
+
+const app = express();
+
+const nocache = (req, res, next) => {
+  res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+  res.header("Expires", "-1");
+  res.header("Pragma", "no-cache");
+  next();
+};
+
+const generateAccessToken = (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  const channelName = req.query.channelName;
+
+  if (!channelName) {
+    return res.status(500).json({ Error: "channel is required" });
+  }
+
+  let uid = req.query.uid;
+  if (!uid || uid == "") {
+    uid = 0;
+  }
+
+  let role = RtcRole.SUBSCRIBER;
+  if (req.query.role == "publisher") {
+    role = RtcRole.PUBLISHER;
+  }
+
+  let expireTime = req.query.expireTime;
+  if (!expireTime || expireTime == "") {
+    expireTime = 3600;
+  } else {
+    expireTime = parseInt(expireTime, 10);
+  }
+
+  const currentTime = Math.floor(Date.now() / 1000);
+  const privilageExpireTime = currentTime + expireTime;
+
+  const token = RtcTokenBuilder.buildTokenWithUid(
+    APP_ID,
+    APP_CERTIFICATE,
+    channelName,
+    uid,
+    role,
+    privilageExpireTime
+  );
+  return res.json({ token: token });
+};
+
+app.get("/access_token", nocache, generateAccessToken);
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});

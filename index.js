7b8f6ef7e30b71eb17eff0f67dc985a82bf29035
Stargazing-11 <samuelkifle199314@gmail.com>
2023-08-19 17:06:53 +0300

    update port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { RtcTokenBuilder, RtcRole } = require("agora-access-token");
 require("dotenv").config();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const APP_ID = process.env.APP_ID;
 const APP_CERTIFICATE = process.env.APP_CERTIFICATE;
 
